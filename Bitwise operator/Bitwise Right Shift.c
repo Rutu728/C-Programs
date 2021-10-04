12cd2d3d2189bb721ffc8967c4c8c61afdabbac9
Rutu728 <72727718+Rutu728@users.noreply.github.com>
2021-10-04 18:58:09 +0530

    Add files via upload

---
+++ Bitwise operator/Bitwise Right Shift.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+  int main()
+  {
+   int No = 0, Res = 0;
+   printf("\n enter a numbres = ");
+   scanf("%d",&No);
+
+   Res = No >> 2 ;
+   printf("\n output of Bitwise Right Shift  => \n\n\t\t %d >> 2 = %d",No,Res);
+   
+   getch();
+   return 0;
+  }
+    
