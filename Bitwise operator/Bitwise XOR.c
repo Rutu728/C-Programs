301e3dcc8c6100f13aedd8b1478b9f8a7b0838f6
Rutu728 <72727718+Rutu728@users.noreply.github.com>
2021-10-04 18:59:20 +0530

    Add files via upload

---
+++ Bitwise operator/Bitwise XOR.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+  int main()
+  {
+   int No1 = 0,No2 = 0, Res = 0;
+   printf("\n enter 2 numbres = ");
+   scanf("%d%d",&No1,&No2);
+
+   Res = No1 ^ No2;
+   printf("\n output of Bitwise XOR between => %d^%d=%d",No1,No2,Res);
+   
+   getch();
+   return 0;
+  }
+    
